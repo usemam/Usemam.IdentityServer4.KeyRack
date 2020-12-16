64c62de079b383f7d014d48df789b52d022004ac
Usein Mambediiev <useinm@gmail.com>
2020-12-15 23:58:47 -0800

    Working on unit tests for KeyService - more tests

--- test/Usemam.IdentityServer4.KeyRack.UnitTests/KeyServiceTests.cs
+++ test/Usemam.IdentityServer4.KeyRack.UnitTests/KeyServiceTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 
@@ -21,7 +22,10 @@ namespace Usemam.IdentityServer4.KeyRack.UnitTests
         private const int KeyExpirationSec = 60;
         private const int KeyRetirementSec = 120;
 
+        private const string RetiredKeyId = "A5C8AA4960F0";
+        private const string ExpiredKeyId = "C7A281B5AC6B";
         private const string ActiveKeyId = "AC2345EF097412";
+        private const string NewKeyId = "FBA456938E26";
 
         private KeyService _service;
         private KeyRackOptions _options;
@@ -58,6 +62,62 @@ namespace Usemam.IdentityServer4.KeyRack.UnitTests
 
             Assert.NotNull(result);
             Assert.Equal(ActiveKeyId, result.KeyId);
+        }
+
+        [Fact]
+        public async void NoKeys_NewKeyCreated()
+        {
+            _repositoryMock
+                .Setup(x => x.LoadKeysAsync())
+                .Returns(Task.FromResult(Enumerable.Empty<SerializedKey>()));
+            var serializedNewKey = new SerializedKey {KeyId = NewKeyId};
+            _serializerMock.Setup(x => x.Serialize(It.IsAny<RsaKey>())).Returns(serializedNewKey);
+
+            var result = await _service.GetCurrentKeyAsync();
+
+            _repositoryMock.Verify(
+                x => x.StoreKeyAsync(It.Is<SerializedKey>(k => k.KeyId == NewKeyId)), Times.Once);
+        }
+
+        [Fact]
+        public async void ExpiredKey_NewKeyCreated()
+        {
+            var created = DateTime.UtcNow.Subtract(TimeSpan.FromSeconds(KeyExpirationSec + 1));
+            var expiredKey = CreateTestKey(ExpiredKeyId, created);
+            var serializedExpiredKey = new SerializedKey {KeyId = ExpiredKeyId};
+            _serializerMock.Setup(x => x.Deserialize(serializedExpiredKey)).Returns(expiredKey);
+
+            var serializedNewKey = new SerializedKey {KeyId = NewKeyId};
+            _serializerMock
+                .Setup(x => x.Serialize(It.Is<RsaKey>(k => k.KeyId != ExpiredKeyId)))
+                .Returns(serializedNewKey);
+
+            _repositoryMock
+                .Setup(x => x.LoadKeysAsync())
+                .Returns(Task.FromResult<IEnumerable<SerializedKey>>(new [] {serializedExpiredKey}));
+            
+            var result = await _service.GetAllKeysAsync();
+
+            _repositoryMock.Verify(
+                x => x.StoreKeyAsync(It.Is<SerializedKey>(k => k.KeyId == NewKeyId)), Times.Once);
+        }
+
+        [Fact]
+        public async void RetiredKey_Deleted()
+        {
+            var created = DateTime.UtcNow.Subtract(TimeSpan.FromSeconds(KeyRetirementSec + 1));
+            var retiredKey = CreateTestKey(RetiredKeyId, created);
+            var serializedRetiredKey = new SerializedKey {KeyId = RetiredKeyId};
+            _serializerMock.Setup(x => x.Deserialize(serializedRetiredKey)).Returns(retiredKey);
+
+            _repositoryMock
+                .Setup(x => x.LoadKeysAsync())
+                .Returns(Task.FromResult<IEnumerable<SerializedKey>>(new [] {serializedRetiredKey}));
+
+            var result = await _service.GetAllKeysAsync();
+
+            _repositoryMock.Verify(
+                x => x.DeleteKeyAsync(RetiredKeyId), Times.AtLeastOnce);
         }
 
         private KeyRackOptions CreateTestOptions() =>
